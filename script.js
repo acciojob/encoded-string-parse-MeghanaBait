e3a8a1c373d49a5691470cc4abf80a1d793f5058
MeghanaBait <133511757+MeghanaBait@users.noreply.github.com>
2023-12-31 10:23:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
 const parseCode = (str) => {
   // your code here
 };
+const parts = str.split('0').filter(Boolean);
+const firstName = parts[0];
+const lastName = parts[1];
+const id = parts[2];
+
+return{
+	firstName: firstName;
+	lastName:lastName;
+id:id;
+};
 
 // Do not change the code below
-const str = prompt("Enter str: ");
-alert(JSON.stringify(parseCode(str)));
+// const str = prompt("Enter str: ");
+// alert(JSON.stringify(parseCode(str)));
