9652e0ece70c172959e5f3222c6729355c9e390e
MeghanaBait <133511757+MeghanaBait@users.noreply.github.com>
2023-12-31 10:29:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
 const parseCode = (str) => {
   // your code here
 	const parts = str.split('0').filter(Boolean);
-const firstName = parts[0];
-const lastName = parts[1];
-const id = parts[2];
 
-return{
-	firstName: firstName,
-	lastName:lastName,
-id:id
-};
+    // Extract the firstName, lastName, and id
+    const firstName = parts[0];
+    const lastName = parts[1];
+    const id = parts[2];
+
+    // Return the parsed data as an object
+    return {
+        firstName: firstName,
+        lastName: lastName,
+        id: id
+    };
+	
 };
 
 
